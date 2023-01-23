9e8e0f71048857f434b7f016a347f8e1e75f8278
Tripp Barker <colemanbarker@gmail.com>
2023-01-23 11:35:54 -0500

    working on day 11...

--- src/Day11/Main.java
+++ src/Day11/Main.java
@@ -2,6 +2,7 @@ package Day11;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -10,15 +11,14 @@ public class Main {
 	public static FileReader fr;
 	
 	public static void main(String[] args) {
+		Monkey monk = new Monkey(10);
 		try {
-			fr = new FileReader("testMonkeys.txt");
+			fr = new FileReader("monkeys.txt");
 			sc = new Scanner(fr);
 		} catch (FileNotFoundException fnfe) {
 			System.out.println(fnfe);
 		}
-		while(sc.hasNext()) {
-			System.out.println(sc.nextLine());
-		}
+		System.out.println(monk);
 	}
 
 }
